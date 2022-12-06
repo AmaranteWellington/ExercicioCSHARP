17d517ce52f485d0b249c1f8464c86802ec6fa28
AmaranteWellington <amaranthwfs@gmail.com>
2022-12-06 14:07:57 -0300

    first commit

---
+++ ProgramCalRet.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Course.Entities;
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo ret = new Retangulo();
+            
+
+            Console.WriteLine("Entre com a Largura e a Altura do Retangulo: ");
+            ret.Largura = double.Parse(Console.ReadLine());
+            ret.Altura = double.Parse(Console.ReadLine());
+
+
+
+            Console.WriteLine("AREA: " + ret.Area());
+            Console.WriteLine("PERIMETRO: " + ret.Perimetro());
+            Console.WriteLine("DIAGONAL: " + ret.Diagonal());
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
